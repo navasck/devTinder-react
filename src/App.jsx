3cd31e3c9c1e9bb809ae53a79f56163ca7e7f273
ABDUL NAVAS CK <cknavas123@gmail.com>
2024-10-18 18:33:57 +0530

    login page & header footer and feed and profile pages arw done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Body from './components/Body';
+import Login from './components/Login';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
+import Feed from './components/Feed';
+import Profile from './components/Profile';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Dev Tinder</h1>
+      <Provider store={appStore}>
+        <BrowserRouter basename='/'>
+          <Routes>
+            <Route path='/' element={<Body />}>
+              <Route path='/' element={<Feed />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/profile' element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
 
-export default App
+export default App;

e1e0e108234458c0bef02d1465e3239687902ab8
ABDUL NAVAS CK <cknavas123@gmail.com>
2025-02-09 18:35:02 +0530

    fix: base url chnaged

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -22,7 +22,7 @@ const UserCard = ({ user }) => {
   return (
     <div className='card bg-base-300 w-96 shadow-xl'>
       <figure>
-        <img src={user.photoUrl} alt='photo' />
+        <img src={photoUrl} alt='photo' />
       </figure>
       <div className='card-body'>
         <h2 className='card-title'>{firstName + ' ' + lastName}</h2>
--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0', // Listen on all available interfaces
+//     port: 5173, // Or your preferred port
+//     hmr: {
+//       clientPort: 5173, // or your port if different from above
+//     },
+//   },
+// });

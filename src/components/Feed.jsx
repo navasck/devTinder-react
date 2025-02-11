516fea8df25ef5703eaa1769cec420e3dff88066
ABDUL NAVAS CK <cknavas123@gmail.com>
2025-02-11 21:45:06 +0530

    feat: swiper carousel added in feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,12 +2,20 @@ import axios from 'axios';
 import { BASE_URL } from '../utils/constants';
 import { useDispatch, useSelector } from 'react-redux';
 import { addFeed } from '../utils/feedSlice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import UserCard from './UserCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 
 const Feed = () => {
   const feed = useSelector((store) => store.feed);
   const dispatch = useDispatch();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const getFeed = async () => {
     if (feed) return;
@@ -22,6 +30,14 @@ const Feed = () => {
   };
 
   useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const slidesPerView = windowWidth < 768 ? 'auto' : 5;
+
+  useEffect(() => {
     getFeed();
   }, []);
   if (!feed) return;
@@ -30,11 +46,35 @@ const Feed = () => {
     return <h1 className='flex justify-center my-10'>No new users founds!</h1>;
 
   return (
-    feed && (
-      <div className='flex justify-center my-10'>
-        <UserCard user={feed[0]} />
+    <>
+      <div className='flex my-10'>
+        {/* <UserCard user={feed[0]} /> */}
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={slidesPerView}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className='mySwiper'
+        >
+          {feed?.map((user) => (
+            <SwiperSlide key={user._id || user.id}>
+              <div className=''>
+                <UserCard user={user} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-    )
+    </>
   );
 };
 export default Feed;
